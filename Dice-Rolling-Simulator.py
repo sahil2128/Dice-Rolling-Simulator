848065eb186db3a77790ecce16f59c29a2b1dd17
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-02-09 23:54:44 +0530

    Update Dice-Rolling-Simulator.py

--- Dice-Rolling-Simulator.py
+++ Dice-Rolling-Simulator.py
@@ -1,13 +1,42 @@
 import random
-min = 1
-max = 6
+while x == "y":
+    number = random.randint(1,6)
 
-roll_again = "yes"
-
-while roll_again == "yes" or roll_again == "y":
-    print ("Rolling the dices...")
-    print( "The values are....")
-    print (random.randint(min, max))
-    print (random.randint(min, max))
-
-    roll_again = input("Roll the dices again?")
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    x = input("Press y to roll again")
+    
